4e1f483a00e90eb8a57bce183d84705bd277e01e
PRIYA9065 <147863167+PRIYA9065@users.noreply.github.com>
2026-05-20 17:42:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 function toFahrenheit(celsius) {
  // Write your code here
 	let fahrenheit = ((celsius * 9.0) / 5) + 32;
-	return fahrenheit;
+	return fahrenheit.toFixed(2);
 }
-
 // Do not change the code below
 const celsius = prompt("Enter Celsius:");
 alert(toFahrenheit(Number(celsius)));
